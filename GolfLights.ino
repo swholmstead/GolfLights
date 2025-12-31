22e36d52bf7012a210546b2ac4274baa0f6dcaa2
swholmstead <swholmstead@gmail.com>
2025-12-31 17:42:53 -0500

    Update GolfLights.ino

--- GolfLights.ino
+++ GolfLights.ino
@@ -26,8 +26,7 @@
 #define numLEDs      116  // number of LEDs used in strip, needs to be an even number
 #define maxBright    127 // 0-255 max brightness; to prevent overcurrent, start low
 #else
-// #define numLEDs       44
-#define numLEDs       36
+#define numLEDs       44
 #define maxBright    255 // 0-255 max brightness; to prevent overcurrent, start low
 #endif
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(numLEDs, ledPin, NEO_GRB + NEO_KHZ800);
